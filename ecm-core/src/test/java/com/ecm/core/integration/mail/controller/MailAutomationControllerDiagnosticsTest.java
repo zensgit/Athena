5d431b05902eb6d6605d56e6ad577ea4f413d9ba
zensgit <77236085+zensgit@users.noreply.github.com>
2026-02-13 11:36:32 +0800

    test(core): align controller and diagnostics tests to current contracts

--- ecm-core/src/test/java/com/ecm/core/integration/mail/controller/MailAutomationControllerDiagnosticsTest.java
+++ ecm-core/src/test/java/com/ecm/core/integration/mail/controller/MailAutomationControllerDiagnosticsTest.java
@@ -7,6 +7,7 @@ import com.ecm.core.integration.mail.repository.ProcessedMailRepository;
 import com.ecm.core.integration.mail.service.MailFetcherService;
 import com.ecm.core.integration.mail.service.MailOAuthService;
 import com.ecm.core.integration.mail.service.MailProcessedRetentionService;
+import com.ecm.core.integration.mail.service.MailReportScheduledExportService;
 import com.ecm.core.integration.mail.service.MailReportingService;
 import com.ecm.core.service.AuditService;
 import com.ecm.core.service.SecurityService;
@@ -61,6 +62,9 @@ class MailAutomationControllerDiagnosticsTest {
 
     @MockBean
     private MailReportingService reportingService;
+
+    @MockBean
+    private MailReportScheduledExportService scheduledExportService;
 
     @MockBean
     private ProcessedMailRepository processedMailRepository;
